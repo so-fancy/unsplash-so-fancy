3c39bc0fc85e4c2f59c6c1fb4d5dd9f29237d71b
Steve Mao <maochenyan@gmail.com>
2016-08-03 21:16:45 +1000

    migrate to listr

--- index.js
+++ index.js
@@ -6,53 +6,75 @@ const fs = require('fs');
 const pathExists = require('path-exists');
 const pify = require('pify');
 const wallpaper = require('wallpaper');
+const Listr = require('listr');
 
 const DEST = path.resolve(process.env.HOME, 'unsplash-so-fancy');
 
 let location;
 let filename;
+let imageExists;
+let response;
+let imagePath;
 
 module.exports = url => {
-  // todo: use listr
-  Promise.all([
-    pify(fs.mkdir)(DEST).catch(_ => {}),
-    fetch(url, { redirect: 'manual' })
-  ])
-  .then(data => {
-    const res = data[1];
-    if (res.status !== 302) {
-      throw Error('No redirect - not sure what the image ID is. Bug seph');
-    }
+    const tasks = new Listr([
+        {
+            title: 'Preparing',
+            task: () => {
+                return new Listr([
+                    {
+                        title: 'Creating image folder if not exist',
+                        task: () => pify(fs.mkdir)(DEST).catch(() => {})
+                    },
+                    {
+                        title: 'Getting image filename',
+                        task: () => fetch(url, { redirect: 'manual' }).then(res => {
+                            if (res.status !== 302) {
+                                throw Error('No redirect - not sure what the image ID is. Bug seph');
+                            }
 
-    location = urllib.resolve('https://source.unsplash.com/', res.headers.get('location'));
-    if (!location) throw Error('No location header');
+                            location = urllib.resolve('https://source.unsplash.com/', res.headers.get('location'));
+                            if (!location) throw Error('No location header');
 
-    filename = path.parse(urllib.parse(location).pathname).base;
+                            filename = path.parse(urllib.parse(location).pathname).base;
+                        })
+                    }
+                ], { concurrent: true });
+            }
+        },
+        {
+            title: 'Checking if image already exists',
+            task: () => Promise.all([pathExists(`${filename}.jpeg`), pathExists(`${filename}.png`)])
+              .then(exist => imageExists = exist[0] || exist[1])
+        },
+        {
+            title: 'Downloading image',
+            task: () => fetch(location).then(res => response = res),
+            skip: () => imageExists
+        },
+        {
+            title: 'Copying image',
+            task: () => {
+                const contenttype = response.headers.get('content-type');
+                const ext = contenttype.indexOf('jpeg') !== -1 ? 'jpeg' : 'png'; // bleh.
 
-    return Promise.all([pathExists(`${filename}.jpeg`), pathExists(`${filename}.png`)]);
-  })
-  .then(exist => {
-    if (exist[0] || exist[1]) {
-      console.log(`Already downloaded ${filename}`);
-    } else {
-      console.log('Fetching', filename);
+                imagePath = path.resolve(DEST, `${filename}.${ext}`);
+                response.body.pipe(fs.createWriteStream(imagePath));
 
-      return fetch(location);
-    }
-  })
-  .then(res => {
-    const contenttype = res.headers.get('content-type');
-    const ext = contenttype.indexOf('jpeg') !== -1 ? 'jpeg' : 'png'; // bleh.
+                return new Promise((resolve, reject) => {
+                    response.body.once('end', () => resolve());
+                    response.body.once('error', () => reject());
+                })
+            },
+            skip: () => imageExists
+        },
+        {
+            title: `setting wallpaper`,
+            task: () => wallpaper.set(imagePath)
+        }
+    ]);
 
-    const p = path.resolve(DEST, `${filename}.${ext}`);
-    res.body.pipe(fs.createWriteStream(p));
-    res.body.once('end', _ => {
-      console.log(`setting ${p} as wallpaper`);
-      wallpaper.set(p)
-      .then(_ => console.log('done'));
+    tasks.run().catch(err => {
+        console.error(err);
     });
-  })
-  .catch(e => {
-    console.error(e.stack);
-  });
 };
